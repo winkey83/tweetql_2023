5c72185d852b998b0e5d846f67a84e074288f31a
winkey83 <winkey83@gmail.com>
2023-03-05 17:44:07 +0900

    work

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
     type User {
-        id:ID
-        username: String
+        id:ID!
+        username: String!
+        firstName: String!
+        lastName: String
     }
     type Tweet {
-        id:ID
-        text:String
-        author: User
+        id:ID!
+        text:String!
+        author: User!
     }
     type Query {
-        allTweets:[Tweet]
-        tweet(id: ID): Tweet
+        allTweets:[Tweet!]!
+        tweet(id: ID!): Tweet
+    }
+    type Mutation {
+        postTweet(text:String!, userId:ID!): Tweet!
+        deleteTweet(id:ID!): Boolean!
     }
 `
 
-const server = new ApolloServer({typeDefs})
+const server = new ApolloServer({ typeDefs })
 
-server.listen().then(({url})=>{
+server.listen().then(({ url }) => {
     console.log(`Running on ${url}`)
 })
